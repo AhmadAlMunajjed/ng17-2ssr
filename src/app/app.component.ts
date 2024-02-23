8f266e79814ad8e0cf98a004c7e105e44194e7fc
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2024-02-23 14:11:46 +0800

    netlify request and Cache http requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, Optional, ViewChild, afterNextRender } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import type { Context } from "@netlify/edge-functions"
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @ViewChild('content') contentRef!: ElementRef;
+
   title = 'ng17-2ssr';
+
+  constructor(
+    @Inject('netlify.request') @Optional() request?: Request,
+    @Inject('netlify.context') @Optional() context?: Context
+  ) {
+    console.log(`Rendering Foo for path ${request?.url} from location ${context?.geo?.city}`)
+
+    afterNextRender(() => {
+      // Safe to check `scrollHeight` because this will only run in the browser, not the server.
+      console.log('content height: ' + this.contentRef.nativeElement.scrollHeight);
+    });
+
+  }
+
 }
